33d25434735e98b3ef92de22a6cdbd6e4218502c
big-o <big-o@users.noreply.github.com>
2022-06-25 00:33:32 +0100

    drawing

--- skdag/__init__.py
+++ skdag/__init__.py
@@ -1,8 +1,11 @@
-from skdag.dag import DAG
+from skdag.dag import *
 
 from skdag._version import __version__
 
 __all__ = [
     "DAG",
+    "DAGBuilder",
+    "DAGDisplay",
+    "DAGStep",
     "__version__",
 ]
